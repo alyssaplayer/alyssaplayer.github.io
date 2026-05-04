661c0f3cf84ba15d48641bc19ebefbc4b8a09964
alyssaplayer <alyssa18player@gmail.com>
2026-05-04 22:41:25 +0000

    Deploying to gh-pages from @ alyssaplayer/alyssaplayer.github.io@a1edcf5f382fa85a610dc81642a236a8c5c30918 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,9 +398,9 @@ ninja.data = [{
           description: "Analysis of Palos Verdes Reef as a refuge for climate-threatened species.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/refugia-ar/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-automated-property-valuation",
+          title: 'Automated Property Valuation',
+          description: "Supervised ML model to predict residential assessed property values using Zillow data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
