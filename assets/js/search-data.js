4373592ee6a66f7c0742f8b91bc35c27b49a60f3
alyssaplayer <alyssa18player@gmail.com>
2026-05-04 21:37:20 +0000

    Deploying to gh-pages from @ alyssaplayer/alyssaplayer.github.io@3ccdf57258d27f3051d3e5e3e9b3882e96b4f264 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-repositories",
+  },{id: "nav-projects",
+          title: "projects",
+          description: "A collection of selected projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-repositories",
           title: "repositories",
           description: "A collection of my repositories. Please visit the README.md files to learn more about these projects.",
           section: "Navigation",
